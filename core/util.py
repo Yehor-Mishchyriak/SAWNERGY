f6792260367d3a767d33cc68de2612b05be28795
Yehor Mishchyriak <yehormishchyriak@gmail.com>
2024-10-13 00:57:21 -0400

    greatly expanded the functionality of the util

--- core/util.py
+++ core/util.py
@@ -1,14 +1,17 @@
 #!AllostericPathwayAnalyzer/venv/bin/python3
 
 import atexit
-import logging.config
+import logging
+from logging.config import dictConfig
+import importlib.util
 import os
 import datetime
-import logging
 import numpy as np
 from json import load
 from re import search
 from concurrent.futures import as_completed
+from collections import Mapping
+from copy import deepcopy
 
 
 #############################
@@ -18,7 +21,10 @@ from concurrent.futures import as_completed
 def _softmax(matrix: np.array, axis=1):
     pass
 
-def transition_probs_from_interactions(matrix: np.array):
+def normalize_vector(vector: np.array):
+    return _softmax(vector)
+
+def probabilities_from_interactions(matrix: np.array):
     return _softmax(matrix)
 
 
@@ -33,7 +39,7 @@ def load_json_config(config_location: str) -> dict:
 
 def set_up_logging(config_location, logger_name):
     config = load_json_config(config_location)
-    logging.config.dictConfig(config)
+    dictConfig(config)
     which_queue_handler = "queue_handler_construction_module" if logger_name == "network_construction_module" else "queue_handler_model"
     queue_handler = logging.getHandlerByName(which_queue_handler)
     if queue_handler is not None:
@@ -77,9 +83,71 @@ def process_elementwise(in_parallel=False, Executor=None):
     
     return inner
 
+class FrozenDict(Mapping):
+    def __init__(self, data):
+        # Store a deep copy of the input dictionary to ensure immutability
+        self._data = deepcopy(data)
+        
+    def __getitem__(self, key):
+        # Return a deep copy of the value to prevent mutation
+        return deepcopy(self._data[key])
+    
+    def __iter__(self):
+        return iter(self._data)
+    
+    def __len__(self):
+        return len(self._data)
+    
+    def __hash__(self):
+        # FrozenDict is hashable based on the items in it, converted to a frozenset
+        return hash(frozenset(self._data.items()))
+    
+    def __repr__(self):
+        return f"FrozenDict({self._data})"
+
 ##################################
 # FILE-SPECIFIC HELPER FUNCTIONS #
 ##################################
+
+def import_network_directory_components(directory_path: str):
+        residues = {}
+        interaction_matrices = {}
+        probability_matrices = {}
+        
+        interaction_matrix_index = 0
+        probability_matrix_index = 0
+
+        # Loop through files in the directory
+        for filename in os.listdir(directory_path):
+            filepath = os.path.join(directory_path, filename)
+            
+            if filename == "__pycache__" or not os.path.isdir(filepath) and not filename.endswith(".py") and not filename.endswith(".npy"):
+                continue
+
+            # Load the residues variable from the Python file
+            if filename.endswith(".py"):
+                spec = importlib.util.spec_from_file_location(filename[:-3], filepath)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                
+                if hasattr(module, 'residues'):
+                    residues = module.residues
+
+            # Loop through .npy files in subdirectories; load interaction and probability matrices
+            elif os.path.isdir(filepath):
+                # Loop through .npy files in subdirectories
+                for npy_file in os.listdir(filepath):
+                    path_to_npy_file = os.path.join(filepath, npy_file)
+                    if npy_file.endswith(".npy"):
+                        matrix = np.load(path_to_npy_file, allow_pickle=True)
+                        if "interactions" in npy_file:
+                            interaction_matrices[interaction_matrix_index] = matrix
+                            interaction_matrix_index += 1
+                        if "probabilities" in npy_file:
+                            probability_matrices[probability_matrix_index]
+                            probability_matrix_index += 1
+        
+        return FrozenDict(residues), FrozenDict(interaction_matrices), FrozenDict(probability_matrices)
 
 def construct_batch_sequence(number_frames, batch_size):
     number_batches, residual_frames = divmod(number_frames, batch_size)
