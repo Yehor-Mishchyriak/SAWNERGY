27ac384d1f83ee1336e742d8d1827c4e50851895
Yehor Mishchyriak <yehormishchyriak@gmail.com>
2025-03-29 21:58:54 -0400

    temporarily removed the docstrings to ease the development process

--- allosteric_pathway_analyzer/analyses_processor.py
+++ allosteric_pathway_analyzer/analyses_processor.py
@@ -19,34 +19,10 @@ class AnalysesProcessor:
         return f"{self.__class__.__name__}(config={self.cls_config})"
 
     def set_config(self, config: dict) -> None:
-        if self.__class__.__name__ not in config:
-            raise ValueError(f"Invalid config: missing {self.__class__.__name__} sub-config")
-        
-        sub_config = config[self.__class__.__name__]
-        if "output_directory_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing output_directory_name field")
-        if "csv_file_header" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing csv_file_header field")
-        if "csv_file_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing csv_file_name field")
-        
         self.global_config = config
         self.cls_config = config[self.__class__.__name__]
 
     def _construct_csv_file_path(self, cpptraj_file_path: str, output_directory: str) -> str:
-        """
-        Construct the output CSV file path based on the cpptraj file name and configuration.
-        
-        This method extracts the frame range from the cpptraj file name and uses the
-        'csv_file_name' template from the configuration to build the CSV file name.
-        
-        Args:
-            cpptraj_file_path (str): Full path to the cpptraj output file.
-            output_directory (str): Directory where the CSV file should be created.
-        
-        Returns:
-            str: The full path to the output CSV file.
-        """
         # Extract frame range from the file name.
         cpptraj_file_name = os.path.basename(cpptraj_file_path)
         start_frame, end_frame = _util.frames_from_name(cpptraj_file_name)
@@ -62,18 +38,6 @@ class AnalysesProcessor:
 
     def cpptraj_to_csv_incrementally(self, cpptraj_file_path: str, output_directory: str,
                                      allowed_memory_percentage_hint: float, num_workers: int) -> None:
-        """
-        Convert a cpptraj output file to CSV format incrementally (by processing file chunks).
-        
-        This method writes the CSV header, then processes the cpptraj file in chunks
-        (to keep memory usage low) and appends each chunk to the CSV file.
-        
-        Args:
-            cpptraj_file_path (str): Path to the cpptraj output file.
-            output_directory (str): Directory where the CSV file will be created.
-            allowed_memory_percentage_hint (float): Fraction (between 0 and 1) of available memory to use throughout the data processing.
-            num_workers (int): Number of workers to use for the data processing
-        """
         csv_file_path = self._construct_csv_file_path(cpptraj_file_path, output_directory)
         # Write the CSV header to the output file.
         _util.write_csv_header(self.cls_config["csv_file_header"], csv_file_path)
@@ -82,15 +46,6 @@ class AnalysesProcessor:
             _util.append_csv_from_cpptraj_electrostatics(chunk, csv_file_path)
 
     def cpptraj_to_csv_immediately(self, cpptraj_file_path: str, output_directory: str) -> None:
-        """
-        Convert a cpptraj output file to CSV format immediately (by reading the whole file).
-        
-        This method reads the entire cpptraj file at once and writes its content to a CSV file.
-        
-        Args:
-            cpptraj_file_path (str): Path to the cpptraj output file.
-            output_directory (str): Directory where the CSV file will be created.
-        """
         csv_file_path = self._construct_csv_file_path(cpptraj_file_path, output_directory)
         # Read the entire cpptraj file.
         contents = _util.read_lines(cpptraj_file_path)
@@ -100,29 +55,6 @@ class AnalysesProcessor:
     def process_target_directory(self, target_directory_path: str,
                                 in_parallel: bool, allowed_memory_percentage_hint: Optional[float] = None,
                                 num_workers: Optional[int] = None, output_directory_path: Optional[str] = None) -> str:
-        """
-        Process all cpptraj output files in a target directory and convert them to CSV format.
-
-        Depending on the 'in_parallel' flag, this method will either process the files concurrently using a ThreadPoolExecutor
-        or sequentially. For parallel processing with incremental (chunked) conversion, both 'allowed_memory_percentage_hint'
-        and 'num_workers' must be provided.
-
-        Args:
-            target_directory_path (str): Directory containing cpptraj output files.
-            in_parallel (bool): If True, process files concurrently.
-            allowed_memory_percentage_hint (float): Memory usage hint (as a fraction between 0 and 1) for incremental processing.
-                Required if in_parallel is True.
-            num_workers (int): Number of worker threads for parallel processing.
-                Required if in_parallel is True.
-            output_directory_path (Optional[str], optional): Directory where CSV files will be saved.
-                If None, a new directory is created using the configured output_directory_name.
-
-        Returns:
-            str: The path to the output directory where CSV files have been created.
-
-        Raises:
-            ValueError: If in_parallel is True and either 'num_workers' or 'allowed_memory_percentage_hint' is not provided.
-        """
         # Determine the output directory.
         output_directory = output_directory_path if output_directory_path else _util.create_output_dir(os.getcwd(), self.cls_config["output_directory_name"])
         # List all files in the target directory (assumes they are all cpptraj output files).
--- allosteric_pathway_analyzer/to_matrices_converter.py
+++ allosteric_pathway_analyzer/to_matrices_converter.py
@@ -12,78 +12,18 @@ from . import _util
 
 class ToMatricesConverter:
     def __init__(self) -> None:
-        """
-        Initialize the ToMatricesConverter with the default configuration.
-
-        The configuration is set using the default configuration from pkg_globals,
-        based on the class name.
-        """
         self.global_config = None
         self.cls_config = None
         self.set_config(pkg_globals.default_config)
 
     def __repr__(self) -> str:
-        """
-        Return a string representation of the ToMatricesConverter instance.
-        
-        Returns:
-            str: A string that represents the instance including its configuration.
-        """
         return f"{self.__class__.__name__}(config={self.cls_config})"
 
     def set_config(self, config: dict) -> None:
-        """
-        Set the global and ToMatricesConverter-specific configuration.
-
-        The configuration must include:
-            - ToMatricesConverter
-                - 'output_directory_name'
-                - 'matrices_directory_name'
-                - 'interactions_matrix_name'
-                - 'probabilities_matrix_name'
-                - 'id_to_res_map_name'
-        
-        Args:
-            config (dict): A configuration dictionary
-        
-        Raises:
-            ValueError: If the required configuration keys are missing
-        """
-        if self.__class__.__name__ not in config:
-            raise ValueError(f"Invalid config: missing {self.__class__.__name__} sub-config")
-        
-        sub_config = config[self.__class__.__name__]
-        if "output_directory_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing output_directory_name field")
-        if "matrices_directory_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing matrices_directory_name field")
-        if "interactions_matrix_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing interactions_matrix_name field")
-        if "probabilities_matrix_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing probabilities_matrix_name field")
-        if "id_to_res_map_name" not in sub_config:
-            raise ValueError(f"Invalid {self.__class__.__name__} sub-config: missing id_to_res_map_name field")
-        
         self.global_config = config
         self.cls_config = config[self.__class__.__name__]
 
     def _construct_matrices_output_paths(self, csv_file_path: str, output_directory: str) -> Tuple[str, str]:
-        """
-        Construct and create output directory paths for the matrices based on the CSV file name.
-
-        The method extracts the frame range from the CSV file name, creates the output
-        directory accordingly, and constructs the file paths for both interactions and
-        probabilities matrices.
-
-        Args:
-            csv_file_path (str): Path to the input CSV file.
-            output_directory (str): Base directory where the output directories will be created.
-        
-        Returns:
-            Tuple[str, str]: A tuple containing:
-                - interactions_output_path (str): Path to save the interactions matrix.
-                - probabilities_output_path (str): Path to save the probabilities matrix.
-        """
         # extract the frames range from the csv file name
         csv_file_name = os.path.basename(csv_file_path)
         start_frame, end_frame = _util.frames_from_name(csv_file_name)
@@ -103,18 +43,6 @@ class ToMatricesConverter:
 
     @staticmethod
     def aggregate_energies(csv_file_path: str) -> pd.DataFrame:
-        """
-        Aggregate energy values from a CSV file by summing them for each residue index pair.
-
-        The method reads the CSV file and groups the data by the "residue_i_index" and
-        "residue_j_index" columns, summing the associated energy values.
-
-        Args:
-            csv_file_path (str): Path to the CSV file.
-        
-        Returns:
-            pd.DataFrame: DataFrame with aggregated energy values.
-        """
         # read the CSV file into a df
         df = pd.read_csv(csv_file_path)
         
@@ -124,23 +52,6 @@ class ToMatricesConverter:
 
     @staticmethod
     def to_interactions_probablities_matrices(aggregated_energies: pd.DataFrame) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Convert aggregated energies DataFrame to symmetric interactions and probabilities matrices.
-
-        The method creates two matrices:
-          - The interactions matrix contains absolute energy values with zero self-interactions.
-          - The probabilities matrix is normalized per row, with the diagonal set to the smallest possible
-            float value for safe logarithm operations.
-
-        Args:
-            aggregated_energies (pd.DataFrame): DataFrame containing aggregated energy values,
-                with columns "residue_i_index", "residue_j_index", and "energy".
-        
-        Returns:
-            Tuple[np.ndarray, np.ndarray]: A tuple containing:
-                - interactions_matrix (np.ndarray): The symmetric interactions matrix.
-                - probabilities_matrix (np.ndarray): The normalized probabilities matrix.
-        """
         # extract indices and energy values
         rows = aggregated_energies["residue_i_index"].to_numpy(dtype=np.intp)
         cols = aggregated_energies["residue_j_index"].to_numpy(dtype=np.intp)
@@ -175,19 +86,6 @@ class ToMatricesConverter:
         return interactions_matrix, probabilities_matrix
 
     def aggregate_convert_save(self, csv_file_path: str, output_directory: str) -> None:
-        """
-        Aggregate energy values from a CSV file, convert them to matrices, and save the matrices.
-
-        This method performs the following steps:
-          1. Aggregates energies from the CSV file.
-          2. Converts the aggregated energies into interactions and probabilities matrices.
-          3. Constructs output file paths based on the CSV file name.
-          4. Saves the matrices using NumPy's np.save function.
-        
-        Args:
-            csv_file_path (str): Path to the CSV file.
-            output_directory (str): Base directory for saving the output matrices.
-        """
         aggregated_energies = ToMatricesConverter.aggregate_energies(csv_file_path)
         interactions_matrix, probaility_matrix = ToMatricesConverter.to_interactions_probablities_matrices(aggregated_energies)
 
@@ -196,13 +94,6 @@ class ToMatricesConverter:
         np.save(probabilities_output_path, probaility_matrix)
 
     def process_multiple_csv_files(self, csv_files_to_process: List[str], output_directory: str) -> None:
-        """
-        Process multiple CSV files by aggregating, converting, and saving matrices for each file.
-
-        Args:
-            csv_files_to_process (List[str]): List of CSV file paths to process.
-            output_directory (str): Base directory where the output matrices will be saved.
-        """
         for file in csv_files_to_process:
             self.aggregate_convert_save(file, output_directory)
 
@@ -210,28 +101,6 @@ class ToMatricesConverter:
                                  in_parallel: bool, allowed_memory_percentage_hint: Optional[float] = None,
                                  num_workers: Optional[int] = None, output_directory_path: Optional[str] = None,
                                  create_id_to_res_map: Optional[bool] = True) -> str:
-        """
-        Process all CSV files in a target directory by converting them to matrices.
-
-        Depending on the `in_parallel` flag, the processing can be performed either in parallel or sequentially.
-
-        Args:
-            target_directory_path (str): Path to the directory containing CSV files.
-            in_parallel (bool): Flag indicating whether to process files in parallel.
-            allowed_memory_percentage_hint (Optional[float]): Hint for allowed memory usage percentage 
-                when processing in parallel. Required if `in_parallel` is True.
-            num_workers (Optional[int]): Number of worker processes for parallel processing.
-                Required if `in_parallel` is True.
-            output_directory_path (Optional[str]): Directory where the output matrices will be saved.
-                If not provided, a default output directory is created.
-            create_id_to_res_map (Optional[bool]): Flag indicating whether to create an IDs to residues map (recommended). Defaults to True. 
-        
-        Returns:
-            str: The path to the output directory where matrices are saved.
-        
-        Raises:
-            ValueError: If parallel processing is requested but required parameters are missing.
-        """
         output_directory = output_directory_path if output_directory_path else _util.create_output_dir(os.getcwd(), self.cls_config["output_directory_name"])
         csv_file_paths = [os.path.join(target_directory_path, file) for file in os.listdir(target_directory_path)]
 
@@ -267,19 +136,6 @@ class ToMatricesConverter:
         return output_directory
 
     def map_id_to_res(self, csv_file_path: str, output_directory_path: Optional[str] = None) -> Tuple[Any, ...]:
-        """
-        Create a mapping from residue IDs to residue labels from a CSV file.
-
-        This method reads the CSV file, sorts the rows by "residue_i_index", and removes duplicates.
-        The resulting mapping (as a tuple) is then saved to a file in the specified output directory.
-
-        Args:
-            csv_file_path (str): Path to the CSV file.
-            output_directory (str): Directory where the mapping file will be saved.
-        
-        Returns:
-            Tuple[Any, ...]: A tuple containing the unique residue labels corresponding to each residue ID.
-        """
         output_directory = output_directory_path if output_directory_path else _util.create_output_dir(os.getcwd(), self.cls_config["output_directory_name"])
         df = pd.read_csv(csv_file_path)
         result = tuple(df.sort_values(by=["residue_i_index"]).drop_duplicates(subset=["residue_i_index"])["residue_i"])
