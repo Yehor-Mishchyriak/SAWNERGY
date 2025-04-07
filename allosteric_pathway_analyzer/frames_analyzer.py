951970143b2d3044747256f1291ba93d283f9618
Yehor Mishchyriak <yehormishchyriak@gmail.com>
2025-04-06 21:39:29 -0400

    almost finished frames_analyzer.py V2
    fixed cpptraj commands (except for the COMs), still need to fix the bug with type-specific directories creation in the process_trajectory function

--- allosteric_pathway_analyzer/frames_analyzer.py
+++ allosteric_pathway_analyzer/frames_analyzer.py
@@ -11,34 +11,51 @@ from . import _util
 
 class FramesAnalyzer:
 
-    _from_md = lambda topology_file, trajectory_file, start_frame, end_frame: (
-                    f"echo 'parm {topology_file}\n"
-                    f"trajin {trajectory_file} {start_frame} {end_frame}\n")
+    # * PROTECTED CLASS FIELDS *
 
-    _elec_analysis = lambda start_res_id, end_res_id, cutoff, output_file_path: (
-                    f"pairwise :{start_res_id}-{end_res_id}"
-                    f":{start_res_id}-{end_res_id}"
-                    f"cuteelec {cutoff} avgout {output_file_path} run'")
-    
-    _vdw_analysis = lambda start_res_id, end_res_id, cutoff, output_file_path: (
-                    f"pairwise :{start_res_id}-{end_res_id}"
-                    f":{start_res_id}-{end_res_id}"
-                    f"cutevdw {cutoff} avgout {output_file_path} run'")
-
-    _hbond_analysis = lambda start_res_id, end_res_id, distance_cutoff, angle_cutoff, output_file_path: (
-                    f"donormask :{start_res_id}-{end_res_id}"
-                    f"acceptormask :{start_res_id}-{end_res_id}"
-                    f"distance {distance_cutoff} angle {angle_cutoff} avgout {output_file_path} run'")
-
-    interaction_analyses = {"elec": _elec_analysis, "vdw": _vdw_analysis, "hbond": _hbond_analysis}
-    com_analysis = lambda start_res_id, end_res_id, output_file_path: FramesAnalyzer.__com_analysis_commands(start_res_id, end_res_id, output_file_path)
-
+    # purpose: extract pairwise electrostatic interactions between residues from a specific range
+    # args: start residue, end residue, interaction energy cutoff value
+    #       (if less than that, the interaction won't be recorded), path to the output file
     @staticmethod
-    def __com_analysis_commands(start_res_id: int, end_res_id: int, output_file_path: str):
+    def _elec_analysis(start_res_id, end_res_id, cutoff, output_file_path):
+        return f"pairwise :{start_res_id}-{end_res_id} :{start_res_id}-{end_res_id} cuteelec {cutoff} avgout {output_file_path}\nrun'"
+    
+    # purpose: extract pairwise van der Waals interactions between residues from a specific range
+    # args: start residue, end residue, interaction energy cutoff value
+    #       (if less than that, the interaction won't be recorded), path to the output file
+    @staticmethod
+    def _vdw_analysis(start_res_id, end_res_id, cutoff, output_file_path):
+        return f"pairwise :{start_res_id}-{end_res_id} :{start_res_id}-{end_res_id} cutevdw {cutoff} avgout {output_file_path}\nrun'"
+
+    # purpose: extract pairwise hydrogen bond interactions between residues from a specific range
+    # args: start residue, end residue, distance cutoff value, angle cutoff value
+    #       (if less than either of these, the interaction won't be recorded), path to the output file
+    @staticmethod
+    def _hbond_analysis(start_res_id, end_res_id, distance_cutoff, angle_cutoff, output_file_path):
+        return (f"donormask :{start_res_id}-{end_res_id} acceptormask :{start_res_id}-{end_res_id} distance {distance_cutoff} angle {angle_cutoff} avgout {output_file_path}\nrun'")
+    
+    # purpose: helper method for the com_analysis function
+    #          that builds a cpptraj command that extracts
+    #          center of the mass of each individual residue from a specific residue range
+    # args: start residue, end residue, template for the path to the output file
+    @staticmethod
+    def _com_analysis_commands(start_res_id: int, end_res_id: int, output_file_path_template: str):
         s = ""
         for res_id in range(start_res_id, end_res_id+1):
-            s += f"vector res_{res_id} center :{res_id} out {output_file_path.format(res_id=res_id)}\n"
+            s += f"vector res_{res_id} center :{res_id} out {output_file_path_template.format(res_id=res_id)}\n"
         return s[:-1] + " run' "
+
+    # * PUBLIC CLASS FIELDS *
+
+    # purpose: load the molecular dynamics files
+    # args: topology file, trajectory file, start frame, end frame
+    @staticmethod
+    def load_data_from(topology_file, trajectory_file, start_frame, end_frame):
+        return (f"echo 'parm {topology_file}\ntrajin {trajectory_file} {start_frame} {end_frame}\n")
+    
+    # storing available interaction analyses in the dict
+    interaction_analyses = {"elec": _elec_analysis, "vdw": _vdw_analysis, "hbond": _hbond_analysis}
+    com_analysis = lambda start_res_id, end_res_id, output_file_path_template: FramesAnalyzer._com_analysis_commands(start_res_id, end_res_id, output_file_path_template)
 
     def __init__(self, cpptraj_abs_path: Optional[str] = None, config: Optional[dict] = None) -> None:
         self.global_config = None
@@ -52,6 +69,10 @@ class FramesAnalyzer:
         self._cpptraj = _util.cpptraj_is_available_at(cpptraj_abs_path)
         if self._cpptraj is None:
             raise FileNotFoundError(f"cpptraj was not found or is inaccessible at {cpptraj_abs_path}")
+        
+        # just storing the piping operator and the cpptraj output suppresion so that
+        # its usage in the instance methods does not make the code verbose
+        self._through_cpptraj = f" | {self._cpptraj}" #> /dev/null 2>&1
 
     @property
     def which_cpptraj(self) -> Optional[str]:
@@ -65,30 +86,33 @@ class FramesAnalyzer:
         self.cls_config = config[self.__class__.__name__]
 
     def extract_residue_interactions(self, start_end_frames: Tuple[int, int], topology_file: str, trajectory_file: str,
-                    interaction_type: str, analysis_kwargs: dict, output_directory: str) -> None:
+                                     interaction_type: str, analysis_kwargs: dict, output_directory: str) -> None:
+        
         start_frame, end_frame = start_end_frames
-        output_file_name = self.cls_config["interactions_file_name"].format(start_frame=start_frame, end_frame=end_frame)
+        output_file_name = self.cls_config["interactions_file_name_template"].format(start_frame=start_frame, end_frame=end_frame)
         output_file_path = os.path.join(output_directory, output_file_name)
 
-        command = "".join([self._from_md(topology_file, trajectory_file, start_frame, end_frame),
-                          self.interaction_analyses[interaction_type](**analysis_kwargs, output_file_path),
-                          f"| {self._cpptraj} > /dev/null 2>&1"])
+        command = "".join([self.load_data_from(topology_file, trajectory_file, start_frame, end_frame),
+                          self.interaction_analyses[interaction_type](**analysis_kwargs, output_file_path=output_file_path),
+                          self._through_cpptraj])
 
         try:
             run(command, check=True, shell=True)
         except SubprocessError as e:
             raise RuntimeError(f"An exception occurred while executing the '{command}' command: {e}")
 
-    def extract_residue_coordinates(self, start_end_frames: Tuple[int, int], start_end_residues: Tuple[int, int], topology_file: str, trajectory_file: str,
-                                    output_directory: str):
-        start_frame, end_frame = start_end_frames
-        start_residue, end_residue = start_end_residues
-        output_file_name = self.cls_config["coordinates_file_name"]
-        output_file_path = os.path.join(output_directory, output_file_name)
+    def extract_residue_coordinates(self, start_end_residues: Tuple[int, int], start_end_frames: Tuple[int, int],
+                                    topology_file: str, trajectory_file: str, output_directory: str):
 
-        command = "".join([self._from_md(topology_file, trajectory_file, start_frame, end_frame),
-                          self.com_analysis(start_residue, end_residue, output_file_path),
-                          f"| {self._cpptraj} > /dev/null 2>&1"])
+        start_residue, end_residue = start_end_residues
+        start_frame, end_frame = start_end_frames
+        output_file_name_template = self.cls_config["coordinates_file_name_template"]
+        output_file_path_template = os.path.join(output_directory, output_file_name_template)
+
+        command = "".join([self.load_data_from(topology_file, trajectory_file, start_frame, end_frame),
+                          self.com_analysis(start_residue, end_residue, output_file_path_template),
+                          self._through_cpptraj])
+        print(command)
 
         try:
             run(command, check=True, shell=True)
@@ -96,27 +120,52 @@ class FramesAnalyzer:
             raise RuntimeError(f"An exception occurred while executing the '{command}' command: {e}")
 
     def process_trajectory(self, topology_file: str, trajectory_file: str,
-                       interaction_types_n_kwargs: dict, number_frames: int,
+                       interaction_types_and_kwargs: dict, number_frames: int,
                        in_parallel: bool, batch_size: Optional[int] = 1,
                        in_one_batch: Optional[bool] = False, plotable: Optional[bool] = False, 
-                       start_end_residues: Tuple[int, int] = None, output_directory_path: Optional[str] = None) -> str:
+                       start_end_residues: Optional[Tuple[int, int]] = None, output_directory_path: Optional[str] = None) -> str:
+        """_summary_
+
+        Args:
+            topology_file (str): _description_
+            trajectory_file (str): _description_
+            interaction_types_and_kwargs (dict): _description_
+            number_frames (int): _description_
+            in_parallel (bool): _description_
+            batch_size (Optional[int], optional): _description_. Defaults to 1.
+            in_one_batch (Optional[bool], optional): _description_. Defaults to False.
+            plotable (Optional[bool], optional): _description_. Defaults to False.
+            start_end_residues (Optional[Tuple[int, int]], optional): _description_. Defaults to None.
+            output_directory_path (Optional[str], optional): _description_. Defaults to None.
+
+        Raises:
+            ValueError: _description_
+
+        Returns:
+            str: _description_
+
+        interaction_types_and_kwargs = {
+            "elec": {start_res_id: <int>, end_res_id: <int>, cutoff: <float>},
+            "vdw": {start_res_id: <int>, end_res_id: <int>, cutoff: <float>},
+            "hbond": {start_res_id: <int>, end_res_id: <int>, distance_cutoff: <float>, angle_cutoff: <float>}
+        }
+        """
         output_directory = (
             output_directory_path if output_directory_path
-            else _util.create_output_dir(os.getcwd(), self.cls_config["output_directory_name"])
+            else _util.create_output_dir(os.getcwd(), self.cls_config["output_directory_name_template"])
         )
+        
+        extract_interactions_from = _util.process_elementwise(in_parallel=in_parallel, Executor=ThreadPoolExecutor, capture_output=False)
 
         # If in_one_batch is True, treat all frames as one batch.
-        effective_batch_size = number_frames if in_one_batch else batch_size
-        batches = _util.construct_batch_sequence(number_frames, effective_batch_size)
-        
-        process_frames = _util.process_elementwise(in_parallel=in_parallel,
-                                            Executor=ThreadPoolExecutor,
-                                            capture_output=False)
-        
+        batch_size = number_frames if in_one_batch else batch_size
+        batches = _util.construct_batch_sequence(number_frames, batch_size)
+
         # extract interactions
-        for interaction_type, analysis_kwargs in interaction_types_n_kwargs:
+        for interaction_type, analysis_kwargs in interaction_types_and_kwargs.items():
+            #! ERROR: you need to actually create this directory: self.cls_config[f"{interaction_type}_directory_name"]!!!
             interaction_type_output_directory = os.path.join(output_directory, self.cls_config[f"{interaction_type}_directory_name"])
-            process_frames(batches,
+            extract_interactions_from(batches,
                             self.extract_residue_interactions,
                             topology_file, trajectory_file,
                             interaction_type, analysis_kwargs,
@@ -125,10 +174,11 @@ class FramesAnalyzer:
         if plotable:
             if start_end_residues is None:
                 raise ValueError("If plotable=True, start_end_residues parameter must be provided")
-            process_frames(...) # I stopped here for now; Need to alter the code a bit, so that
-                                # it's required to provide an iterable of residues to compute COMs for
-
-
+            self.extract_residue_coordinates(start_end_residues=start_end_residues,
+                                             start_end_frames=(1, number_frames),
+                                             topology_file=topology_file,
+                                             trajectory_file=trajectory_file,
+                                             output_directory=output_directory)
 
         return output_directory
 
