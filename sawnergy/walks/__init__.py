6d9101e43946ad5f914c88b88cc3b56d5727b027
Yehor Mishchyriak <yehormishchyriak@gmail.com>
2025-10-20 20:46:07 -0400

    reworked imports (forgot to push the first time)

--- sawnergy/walks/__init__.py
+++ sawnergy/walks/__init__.py
@@ -1,3 +1,16 @@
 # walker
-from .walker import *
-from .walker_util import *
+from .walker import Walker
+from .walker_util import (
+    SharedNDArray,
+    l1_norm,
+    apply_on_axis0,
+    cosine_similarity
+)
+
+__all__ = [
+    "Walker",
+    "SharedNDArray",
+    "l1_norm",
+    "apply_on_axis0",
+    "cosine_similarity"
+]
